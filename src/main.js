3d4890bd5e4c839dd16e52794429aa2bc2b16d12
Grimsley Rojas <grimsleyrojas@gmail.com>
2022-05-20 14:41:17 -0400

    añadido direccion del server

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
-axios.defaults.baseURL = 'http://localhost:8000/api';
+axios.defaults.baseURL = 'https://panasgold.com/dashboard/public/api';
 
 import Zendesk from '@dansmaculotte/vue-zendesk'
 
@@ -17,13 +17,13 @@ Vue.use(Zendesk, {
   key: 'f94a30a1-0548-4f3a-b372-25c8972e5666',
   disabled: false,
   hideOnLoad: false,
-  settings: {
-    webWidget: {
-      color: {
-        theme: '#78a300'
-      }
-    }
-  }
+  // settings: {
+  //   webWidget: {
+  //     color: {
+  //       theme: '#78a300'
+  //     }
+  //   }
+  // }
 })
 
 new Vue({
