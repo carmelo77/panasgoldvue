7c99bc3bef6858247aed0526409179aa5c29a412
Grimsley Rojas <grimsleyrojas@gmail.com>
2022-05-20 14:43:35 -0400

    añadido direccion del server en constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const BASE_HREF_IMAGE = "http://127.0.0.1:8000";
+const BASE_HREF_IMAGE = "https://panasgold.com/dashboard/public";
 // const BASE_HREF_IMAGE = "https://panasgold.com/panel/api";
 export {
     BASE_HREF_IMAGE
